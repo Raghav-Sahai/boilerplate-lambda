0b1955c21f7416064869f8a5dba2d76218cc94e0
Raghav Sahai <raghav.sahai21@gmail.com>
2021-10-06 23:18:02 -0400

    update ses service

--- src/services/interfaces/sesInterface.js
+++ src/services/interfaces/sesInterface.js
@@ -3,16 +3,16 @@ const AWS = require('aws-sdk');
 
 const ses = new AWS.SES({ region: config.get('aws.region') });
 
-exports.sendSesMessage = async () => {
+exports.sendSesMessage = async (message, recipientEmail) => {
     console.log("sesInterface::sendSesMessage")
 
     const params = {
         Destination: {
-          ToAddresses: ["Recipient@email.com"]
+          ToAddresses: [recipientEmail]
         },
         Message: {
           Body: {
-            Text: { Data: "Test" }
+            Text: { Data: message }
           },
           Subject: { Data: "Test Email" },
         },
