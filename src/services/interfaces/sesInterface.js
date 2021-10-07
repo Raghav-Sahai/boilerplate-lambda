f9d805a520308e454c87c03da5741f878a9e56cd
Raghav Sahai <raghav.sahai21@gmail.com>
2021-10-06 23:15:48 -0400

    boilerplate ses

--- src/services/interfaces/sesInterface.js
+++ src/services/interfaces/sesInterface.js
@@ -1,6 +1,23 @@
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'REGION'});
+const config = require('config')
+const AWS = require('aws-sdk');
+
+const ses = new AWS.SES({ region: config.get('aws.region') });
 
 exports.sendSesMessage = async () => {
     console.log("sesInterface::sendSesMessage")
+
+    const params = {
+        Destination: {
+          ToAddresses: ["Recipient@email.com"]
+        },
+        Message: {
+          Body: {
+            Text: { Data: "Test" }
+          },
+          Subject: { Data: "Test Email" },
+        },
+        Source: "SourceEmailAddress"
+    };
+
+    return ses.sendEmail(params).promise()
 }
--- src/services/sendMessages.js
+++ src/services/sendMessages.js
@@ -3,6 +3,9 @@ const sesInterface = require('./interfaces/sesInterface')
 exports.sendMessages = async (message, interfaces) => {
     console.log("sendMessages::sendMessages(message, interfaces)", message, interfaces)
 
+    const { email } = interfaces
+
     // TODO: If multiple interfaces, wrap in promise.allSettled?
-    await sesInterface.sendSesMessage()
+
+    await sesInterface.sendSesMessage(message, email)
 }
